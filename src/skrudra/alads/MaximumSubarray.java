ab0c8d0830efd08b56aee70b1b35a6fb8b91443d
Sandeep <skrudra12@gmail.com>
2025-11-19 16:56:28 -0600

    Maximum Subarray

---
+++ src/skrudra/alads/MaximumSubarray.java
@@ -0,0 +1,18 @@
+package skrudra.alads;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i : arr) {
+            currSum += i;
+            maxSum = Math.max(maxSum, currSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        System.out.println(maxSum);
+    }
+}
